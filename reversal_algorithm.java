8e5192fbf5055acadb1a2707d603a874d3fadefa
shilpahassan <shilpaygowda@gmail.com>
2020-07-12 20:31:11 +0530

    Solution for reversal_algorithm (11/07/2020)

---
+++ reversal_algorithm.java
@@ -0,0 +1,44 @@
+import java.io.*; 
+  
+class reversal_algorithm { 
+  
+    static void leftRotate(int arr[], int d) 
+    { 
+  
+        if (d == 0) 
+            return; 
+        int n = arr.length; 
+        rvereseArray(arr, 0, d - 1); 
+        rvereseArray(arr, d, n - 1); 
+        rvereseArray(arr, 0, n - 1); 
+    } 
+  
+   
+    static void rvereseArray(int arr[], int start, int end) 
+    { 
+        int temp; 
+        while (start < end) { 
+            temp = arr[start]; 
+            arr[start] = arr[end]; 
+            arr[end] = temp; 
+            start++; 
+            end--; 
+        } 
+    } 
+  
+    static void printArray(int arr[]) 
+    { 
+        for (int i = 0; i < arr.length; i++) 
+            System.out.print(arr[i] + " "); 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 }; 
+        int n = arr.length; 
+        int d = 3; 
+              d = d % n; 
+        leftRotate(arr, d); 
+        printArray(arr); 
+    } 
+} 
